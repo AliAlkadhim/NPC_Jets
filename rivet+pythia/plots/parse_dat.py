abcc1d25ca6ac8a3cf791f933e4e437e8ef9b23a
AliAlkadhim <aa18dg@my.fsu.edu>
2022-07-13 18:02:37 +0200

    error bars

--- rivet+pythia/plots/parse_dat.py
+++ rivet+pythia/plots/parse_dat.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 
 #ASSUMING EVERYTHING is in /RAW/CMS_2021_I1972986/  , for example /RAW/CMS_2021_I1972986/d23-x01-y01
 
-MAP_DICT = { 
+MAP_DICT_AK4 = { 
     #AK4 JETS
     'd01-x01-y01' : {'y_range':(0,0.5), 
                                 'n_bins': 22,
@@ -35,9 +35,12 @@ MAP_DICT = {
 
    'd04-x01-y01': {'y_range':(1.5,2), 
                                 'n_bins': 16, #15 for ordinary, 16 for RAW
-                                'ylabel': 'AK4 $1.5<|y|<2.0$'},
+                                'ylabel': 'AK4 $1.5<|y|<2.0$'}
+}
 
-   
+
+
+MAP_DICT_AK7 = {
 
    #AK 7
     'd21-x01-y01': {'y_range':(0,0.5), 
@@ -59,6 +62,40 @@ MAP_DICT = {
 
 }
 
+MAP_DICT = { 
+    #AK4 JETS
+    'd01-x01-y01' : {'y_range':(0,0.5), 
+                                'n_bins': 22,
+                                'ylabel':'AK4 $0<|y|<0.5$'},
+
+ 'd02-x01-y01' :  {'y_range':(0.5,1), 
+                                'n_bins': 21,
+                                'ylabel':'AK4 $0.5<|y|<1.0$'},
+
+  'd03-x01-y01':  {'y_range':(1,1.5), 
+                                'n_bins': 19,
+                                'ylabel': 'AK4 $1.0<|y|<1.5$'},
+
+   'd04-x01-y01': {'y_range':(1.5,2), 
+                                'n_bins': 16, #15 for ordinary, 16 for RAW
+                                'ylabel': 'AK4 $1.5<|y|<2.0$'},
+   #AK 7
+    'd21-x01-y01': {'y_range':(0,0.5), 
+                                'n_bins': 22,
+                                'ylabel':'AK7 $0<|y|<0.5$'},
+    
+    'd22-x01-y01': {'y_range':(0.5,1), 
+                                'n_bins': 21,
+                                'ylabel':'AK7 $0.5<|y|<1.0$'},
+
+    'd23-x01-y01': {'y_range':(1,1.5), 
+                                'n_bins': 19,
+                                'ylabel': 'AK7 $1.0<|y|<1.5$'},
+
+    'd24-x01-y01': {'y_range':(1.5,2), 
+                                'n_bins': 16, #15 for ordinary, 16 for RAW
+                                'ylabel': 'AK7 $1.5<|y|<2.0$'}
+}
 begin_file_string = 'CMS_2021_I1972986_'
 # 300 AND 600 BORNKTMIN WITH 800 SUPPR
 # begin_post_hist_string ='BEGIN HISTO1D /suppr800_bornktmin600_100M_posthadron_merged.yoda/CMS_2021_I1972986/'
@@ -85,6 +122,11 @@ def return_bins_pre_post(one_hist):
         bins_list=[]
         pre_entries_list=[]
         post_entries_list = []
+        pre_errors_list = []
+        post_errors_list=[]
+        #Errors are calculated with propagation of errors
+        # Delta (post/pre) = |post/pre| sqrt{([Delta post]/post)^2 + ([Delta pre]/pre)^2 }
+        #where delta is the uncertainties (errors)
         f_readlines=f.readlines()
         for line_ind, line in enumerate(f_readlines):
             
@@ -98,6 +140,8 @@ def return_bins_pre_post(one_hist):
                     bins_list.append(bin_val)
                     pre_entries_val =  f_readlines[begin_pre_table_ind].split()[2]
                     pre_entries_list.append(pre_entries_val)
+                    pre_error =  f_readlines[begin_pre_table_ind].split()[3]
+                    pre_errors_list.append(pre_error)
                     begin_pre_table_ind +=1 
             #POST
             if begin_post_hist_string in line:
@@ -107,31 +151,66 @@ def return_bins_pre_post(one_hist):
                     #bins already fetcheds
                     post_entries_val =  f_readlines[begin_post_table_ind].split()[2]
                     post_entries_list.append(post_entries_val)
+                    post_error =  f_readlines[begin_post_table_ind].split()[3]
+                    post_errors_list.append(post_error)
                     begin_post_table_ind +=1 
     bins, pre, post = np.array(bins_list, dtype='float64'),  np.array(pre_entries_list, dtype='float64') + 1.e-9 ,  np.array(post_entries_list, dtype='float64')
-    print('BINS', bins)
-    print('PRE', pre)
-    print('POST', post)
-    
-    return bins, pre, post
+    pre_errors = np.array(pre_errors_list, dtype='float64') + 1.e-9
+    post_errors = np.array(post_errors_list, dtype='float64') + 1.e-9 
+    # print('BINS', bins)
+    # print('PRE', pre)
+    # print('POST', post)
+    # print('PRE ERRORS', pre_errors)
+    return bins, pre, post, pre_errors, post_errors
 
 
 
 
 if not args.Matrix:
-    for hist_ind, hist in enumerate(MAP_DICT.keys()):
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(20,10))
+    for hist_ind_4, hist_4 in enumerate(MAP_DICT_AK4.keys()):
 
-        bins, pre, post = return_bins_pre_post(hist)
-        NPC = post/pre
+        bins_4, pre_4, post_4, pre_error_4 , post_error_4 = return_bins_pre_post(hist_4)
+        NPC_4 = post_4/pre_4
         
-        plt.step(bins, NPC, label=MAP_DICT[hist]['ylabel'], where='mid')
-        plt.xlabel('$p_T$ [GeV]')
-        plt.ylabel(r'$\frac{\sigma^{PS+MPI+HAD}}{\sigma^{PS}}$')
-        plt.title('bornktmin 10, bornsuppfact 250',font='MonoSpace')
-        plt.ylim(-0.5,3)
-        plt.legend()
-        plt.savefig(args.D+'/ALLBINS_'+args.D+'.png')
+        print('pre_error_4', pre_error_4)
+        print('post_error_4', post_error_4)
+        print('pre_4', pre_4)
+        print('post_4', post_4)
+        # Delta (post/pre) = |post/pre| sqrt{([Delta post]/post)^2 + ([Delta pre]/pre)^2 }
+        factor_4 = np.abs(np.divide(post_4,pre_4))
+        error_NPC_4 = factor_4 * np.sqrt((post_error_4/post_4)**2 + (pre_error_4/pre_4)**2)
+        print('error_NPC_4',error_NPC_4)
 
+        axs[0].step(bins_4, NPC_4, label=MAP_DICT_AK4[hist_4]['ylabel'], where='mid')
+        axs[0].errorbar(bins_4, NPC_4, yerr = error_NPC_4)
+        axs[0].set_xlabel('$p_T$ [GeV]')
+        axs[0].set_ylabel(r'$\frac{\sigma^{PS+MPI+HAD}}{\sigma^{PS}}$', fontsize=30)
+        axs[0].set_title('bornktmin 10, bornsuppfact 250',font='MonoSpace')
+        axs[0].set_ylim(0.1,2.8)
+        axs[0].legend()
+        
+    for hist_ind_7, hist_7 in enumerate(MAP_DICT_AK7.keys()):
+
+        bins_7, pre_7, post_7, pre_error_7, post_error_7  = return_bins_pre_post(hist_7)
+        NPC_7 = post_7/pre_7
+        
+
+        factor_7 = np.abs(np.divide(post_7,pre_7))
+        error_NPC_7 = factor_7 * np.sqrt((post_error_7/post_7)**2 + (pre_error_7/pre_7)**2)
+        print('error_NPC_7',error_NPC_7)
+
+
+        axs[1].step(bins_7, NPC_7, label=MAP_DICT_AK7[hist_7]['ylabel'], where='mid')
+        axs[1].set_xlabel('$p_T$ [GeV]')
+        axs[1].set_ylabel(r'$\frac{\sigma^{PS+MPI+HAD}}{\sigma^{PS}}$', fontsize=30)
+        axs[1].set_title('bornktmin 10, bornsuppfact 250',font='MonoSpace')
+        axs[1].set_ylim(0.1,2.8)
+        axs[1].legend()
+        
+        
+    # plt.savefig(args.D+'/ALLBINS_'+args.D+'.png')
+    # plt.tight_layout()
     plt.show()
 
 elif args.Matrix:
